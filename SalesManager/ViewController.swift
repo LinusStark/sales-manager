8b93dbea4c0f2a190b8398724f947588c38b7b81
LinusStark <454646793@qq.com>
2017-11-14 11:20:45 +0800

    Initial Commit

---
+++ SalesManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SalesManager
+//
+//  Created by Linus on 2017/11/14.
+//  Copyright © 2017年 Linus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
